269543275019aa772410d2c856f55961c66b40f7
buVanchu <regush_02@mail.ru>
2023-10-04 12:48:44 +0300

    fv

--- settings.sql
+++ settings.sql
@@ -1,8 +1,4 @@
--- FUNCTION: public.update_or_insert_data(date, double precision)
-
--- DROP FUNCTION IF EXISTS public.update_or_insert_data(date, double precision);
-
-CREATE OR REPLACE FUNCTION public.update_or_insert_data(
+CREATE OR REPLACE FUNCTION public.update_or_insert_data_cons(
 	date_param date,
 	power_param double precision)
     RETURNS void
@@ -13,13 +9,11 @@ AS $BODY$
 BEGIN
     IF EXISTS (SELECT 1 FROM predicted_power WHERE date_stamp = date_param) THEN
         UPDATE predicted_power
-        SET generated_power = power_param
+        SET consumed_power = power_param
         WHERE date_stamp = date_param;
     ELSE
-        INSERT INTO predicted_power (date_stamp, generated_power, user_id)
+        INSERT INTO predicted_power (date_stamp, consumed_power, user_id)
         VALUES (date_param, power_param, 1);
     END IF;
 END;
 $BODY$;
-
-alter table public.predicted_power add primary key (id);
